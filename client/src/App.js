f41a00ef1f5d890d98228eb2199635d8978cb853
Megan Lu <meganlu@live.com>
2021-03-20 23:32:19 -0400

    set up necessary folders and files

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import './Route.js';
+import './TextBox.js';
+import Route from "./Route";
+
+
+function App() {
+  return (
+      <div className = 'App'>
+        <Route/>
+      </div>
+      /*
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>*/
+  );
+}
+
+export default App;
