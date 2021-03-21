bc6536affb6c15997a744474029596b1e3204d52
Megan Lu <meganlu@live.com>
2021-03-20 23:57:19 -0400

    added kdtree related classes

---
+++ server/src/main/java/edu/brown/cs/abahl1elakhotimlu39skothar7/KDTree.java
@@ -0,0 +1,19 @@
+package edu.brown.cs.abahl1elakhotimlu39skothar7;
+
+import java.util.List;
+
+public class KDTree<T> {
+  public KDTree(List<T> nodes) {
+    this.buildTree(nodes);
+  }
+
+  public void buildTree(List<T> nodes) {
+
+  }
+
+  public void nearest() {
+  }
+
+  //public void add(KDNode n) {}
+  //public void remove(KDNode n) {}
+}
