f87abfb71cf6d19c1a58521102696d78beba4f01
Anika Bahl <anikabahl@anikas-air.devices.brown.edu>
2021-03-28 20:24:00 -0400

    added the KDTree add method

--- server/src/main/java/edu/brown/cs/abahl1elakhotimlu39skothar7/KDNode.java
+++ server/src/main/java/edu/brown/cs/abahl1elakhotimlu39skothar7/KDNode.java
@@ -1,9 +1,14 @@
 package edu.brown.cs.abahl1elakhotimlu39skothar7;
 
 public interface KDNode {
-  //gets current dimension of node? (time, equip, difficulty, % abs, % arms, etc.)
+
+  //gets number of dimensions in node?
+  // current dimension of node? (time, equip, difficulty, % abs, % arms, etc.)
   int getDim();
+
   //gets overall metric of node? (...hashmap? ab-->20%, arms: 30%, ....difficult: 8...)
-  double getMetric();
-  boolean getFlexibility();
+  double getMetric(int dimLevel);
+
+  boolean getFlexibility(int dimLevel);
+
 }
--- server/src/main/java/edu/brown/cs/abahl1elakhotimlu39skothar7/KDTree.java
+++ server/src/main/java/edu/brown/cs/abahl1elakhotimlu39skothar7/KDTree.java
@@ -1,19 +1,73 @@
 package edu.brown.cs.abahl1elakhotimlu39skothar7;
 
+import java.util.Comparator;
 import java.util.List;
 
-public class KDTree<T> {
-  public KDTree(List<T> nodes) {
-    this.buildTree(nodes);
+public class KDTree {
+  private KDNode root;
+  private KDTree left;
+  private KDTree right;
+
+  public KDTree() {
+    this.root = null;
+    this.left = null;
+    this.right = null;
   }
 
-  public void buildTree(List<T> nodes) {
+  public KDTree(List<KDNode> nodes) {
+    this.buildTree(nodes, 0);
+  }
 
+  public void buildTree(List<KDNode> nodes, int curDim) throws RuntimeException {
+    boolean validList = true;
+    int numDim = nodes.get(0).getDim();
+    for (int i = 0; i < nodes.size(); i++) {
+      validList = validList && (nodes.get(i).getDim() == numDim);
+    }
+    if (validList) {
+      nodes.sort(new Comparator<KDNode>() {
+        @Override
+        public int compare(KDNode kdn1, KDNode kdn2) {
+          if (kdn1.getMetric(curDim % numDim) > kdn2.getMetric(curDim % numDim)) {
+            return 1;
+          } else if (kdn1.getMetric(curDim % numDim) < kdn2.getMetric(curDim % numDim)) {
+            return -1;
+          } else {
+            return 0;
+          }
+        }
+      });
+      if (nodes.size() == 1) {
+        this.root = nodes.get(0);
+        this.left = null;
+        this.right = null;
+      } else if (nodes.size() > 1) {
+        int curMid = (nodes.size() - 1) / 2;
+        this.root = nodes.get(curMid);
+        this.left.buildTree(nodes.subList(0, curMid), curDim + 1);
+        this.right.buildTree(nodes.subList(curMid + 1, nodes.size()), curDim + 1);
+      }
+    } else {
+      throw new RuntimeException("ERROR: the list of KDNodes passed to the KDTree was invalid");
+    }
   }
 
   public void nearest() {
   }
 
-  //public void add(KDNode n) {}
-  //public void remove(KDNode n) {}
+  public void add(KDNode n, int curDim) {
+    if (this.root == null) {
+      this.root = n;
+    } else {
+      if (n.getMetric(curDim) < this.root.getMetric(curDim)) {
+        this.left.add(n, curDim + 1);
+      } else {
+        this.right.add(n, curDim + 1);
+      }
+    }
+  }
+
+  public void remove(KDNode n) {
+    
+  }
 }
